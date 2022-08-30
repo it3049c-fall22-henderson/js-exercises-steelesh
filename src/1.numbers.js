c5cade93b0cd4454653d4b52d7e9d20a325faf73
Steele <shrevesa@mail.uc.edu>
2022-08-30 14:34:39 -0400

    fixed finished test: addAndReturn2DecimalPlaces

--- src/1.numbers.js
+++ src/1.numbers.js
@@ -21,7 +21,8 @@ function parseInt(str) {
 //addAndReturn2DecimalPlaces: should return a number value, rounded to 2 decimal places
 // Example: 1.23453 + 5.37873 should return 6.61 and the return type should be a number
 function addAndReturn2DecimalPlaces(num1, num2) {
-  return parseInt((num1 + num2).toFixed(2));
+  const num = num1 + num2;
+  return Number(num.toFixed(2));
 }
 
 module.exports = {
