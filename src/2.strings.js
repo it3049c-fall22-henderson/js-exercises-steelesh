cff949bfabe525f4aa8b0cafe6b3af328ef85bfb
Steele <shrevesa@mail.uc.edu>
2022-08-30 14:08:58 -0400

    finished test: reverseString

--- src/2.strings.js
+++ src/2.strings.js
@@ -2,29 +2,30 @@
 // Example: "abc" => "cba"
 function reverseString(str) {
   /**
-   * can be achieved by combining: 
+   * can be achieved by combining:
    *    1. string.prototype.split
    *    2. array.prototype.reverse
    *    3. array.prototype.join
-   * 
+   *
    * Can even be done on one line
    */
-  return ;
+  const rev = str.split(``).reverse().join(``);
+  return rev;
 }
 
 //capitalize: should return the input in all-caps
 // Example: "this is a string" => "THIS IS A STRING"
 function capitalize(str) {
-  return ;
+  return;
 }
 
 // splitString: should divide a string into substrings and return an array
 // Example: "Jane,Doe,21"  =>  [ "Jane", "Doe", "21" ]
 function splitString(str, splitAt = `,`) {
-  return ;
+  return;
 }
 module.exports = {
   reverseString,
   capitalize,
-  splitString
+  splitString,
 };
